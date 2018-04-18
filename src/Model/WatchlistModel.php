c4b9f88de2d77aa240406cdce0b88e0b73cd9bf4
Konstantin Wagner <k.wagner@heimrich-hannot.de>
2018-04-18 10:18:58 +0200

    added findByUserGroup

--- src/Model/WatchlistModel.php
+++ src/Model/WatchlistModel.php
@@ -10,6 +10,8 @@ namespace HeimrichHannot\WatchlistBundle\Model;
 
 use Contao\Model;
 use Contao\System;
+use HeimrichHannot\Haste\Dca\Member;
+use HeimrichHannot\UtilsBundle\Database\DatabaseUtil;
 
 class WatchlistModel extends Model
 {
@@ -19,80 +21,54 @@ class WatchlistModel extends Model
 
     public function findModelInstanceByPk($pk, array $options = [])
     {
-        return System::getContainer()->get('huh.utils.model')->findModelInstanceByPk(
-            static::$strTable,
-            $pk,
-            $options
-        );
+        return System::getContainer()->get('huh.utils.model')->findModelInstanceByPk(static::$strTable, $pk, $options);
     }
 
     public function findOnePublishedById($id, array $options = [])
     {
-        return System::getContainer()->get('huh.utils.model')->findOneModelInstanceBy(
-            static::$strTable,
-            [static::$strTable.'.id=?', static::$strTable.'.published=?'],
-            [$id, 1],
-            $options
-        );
+        return System::getContainer()->get('huh.utils.model')->findOneModelInstanceBy(static::$strTable, [static::$strTable.'.id=?', static::$strTable.'.published=?'], [$id, 1], $options);
     }
 
     public function findOnePublishedByPid($pid, array $options = [])
     {
-        return System::getContainer()->get('huh.utils.model')->findOneModelInstanceBy(
-            static::$strTable,
-            [static::$strTable.'.pid=?', static::$strTable.'.published=?'],
-            [$pid, 1],
-            $options
-        );
+        return System::getContainer()->get('huh.utils.model')->findOneModelInstanceBy(static::$strTable, [static::$strTable.'.pid=?', static::$strTable.'.published=?'], [$pid, 1], $options);
     }
 
     public function findPublishedByPids(array $ids, array $options = [])
     {
-        return System::getContainer()->get('huh.utils.model')->findModelInstancesBy(
-            static::$strTable,
-            [static::$strTable.'.pid IN(?)', static::$strTable.'.published=?'],
-            [implode(',', $ids), 1],
-            $options
-        );
+        return System::getContainer()->get('huh.utils.model')->findModelInstancesBy(static::$strTable, [static::$strTable.'.pid IN(?)', static::$strTable.'.published=?'], [implode(',', $ids), 1], $options);
     }
 
     public function findByHashAndName(string $hash, string $name, array $options = [])
     {
-        return System::getContainer()->get('huh.utils.model')->findModelInstancesBy(
-            static::$strTable,
-            [static::$strTable.'.hash=?', static::$strTable.'.name=?'],
-            [$hash, $name],
-            $options
-        );
+        return System::getContainer()->get('huh.utils.model')->findModelInstancesBy(static::$strTable, [static::$strTable.'.hash=?', static::$strTable.'.name=?'], [$hash, $name], $options);
     }
 
     public function findPublishedByUuid($uuid, array $options = [])
     {
-        return System::getContainer()->get('huh.utils.model')->findModelInstancesBy(
-            static::$strTable,
-            [static::$strTable.'.uuid=UNHEX(?)', static::$strTable.'.published=?'],
-            [$uuid, 1],
-            $options
-        );
+        return System::getContainer()->get('huh.utils.model')->findModelInstancesBy(static::$strTable, [static::$strTable.'.uuid=UNHEX(?)', static::$strTable.'.published=?'], [$uuid, 1], $options);
     }
 
     public function findOnePublishedBySessionId($sessionId, array $options = [])
     {
-        return System::getContainer()->get('huh.utils.model')->findModelInstancesBy(
-            static::$strTable,
-            [static::$strTable.'.sessionID=?', static::$strTable.'.published=?'],
-            [$sessionId, 1],
-            $options
-        );
+        return System::getContainer()->get('huh.utils.model')->findModelInstancesBy(static::$strTable, [static::$strTable.'.sessionID=?', static::$strTable.'.published=?'], [$sessionId, 1], $options);
     }
 
     public function findByNameAndPid($name, $pid, array $options = [])
     {
-        return System::getContainer()->get('huh.utils.model')->findOneModelInstanceBy(
-            static::$strTable,
-            [static::$strTable.'.name=?', static::$strTable.'.pid=?'],
-            [$name, $pid],
-            $options
-        );
+        return System::getContainer()->get('huh.utils.model')->findOneModelInstanceBy(static::$strTable, [static::$strTable.'.name=?', static::$strTable.'.pid=?'], [$name, $pid], $options);
+    }
+
+    public function findByUserGroups(array $groups, array $options = [])
+    {
+        $user = System::getContainer()->get('huh.utils.member')->findActiveByGroups($groups);
+
+        if (null === $user) {
+            return null;
+        }
+
+        $pids = System::getContainer()->get('huh.utils.database')->computeCondition('pid', DatabaseUtil::OPERATOR_IN, implode(',', $user->fetchEach('id')));
+
+        return System::getContainer()->get('huh.utils.model')->findOneModelInstanceBy(static::$strTable, [$pids[0], 'published=1'], [], $options);
     }
 }
