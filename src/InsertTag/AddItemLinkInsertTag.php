6726447aed0d022214c5a3edf8d8d6141a0854df
Thomas Körner <t.koerner@heimrich-hannot.de>
2025-11-18 16:18:50 +0100

    add entity inserttag support

--- src/InsertTag/AddItemLinkInsertTag.php
+++ src/InsertTag/AddItemLinkInsertTag.php
@@ -6,6 +6,7 @@ use Contao\CoreBundle\DependencyInjection\Attribute\AsInsertTag;
 use Contao\CoreBundle\InsertTag\InsertTagResult;
 use Contao\CoreBundle\InsertTag\OutputType;
 use Contao\CoreBundle\InsertTag\ResolvedInsertTag;
+use Contao\CoreBundle\InsertTag\ResolvedParameters;
 use Contao\CoreBundle\InsertTag\Resolver\InsertTagResolverNestedResolvedInterface;
 use HeimrichHannot\WatchlistBundle\Generator\WatchlistLinkGenerator;
 
@@ -20,9 +21,6 @@ class AddItemLinkInsertTag implements InsertTagResolverNestedResolvedInterface
 
     public function __invoke(ResolvedInsertTag $insertTag): InsertTagResult
     {
-//        {{watchlist_add_item_link::file::<file uuid (string)>::<optional: title>::<optional: watch list uuid>}}
-//        {{watchlist_add_item_link::entity::<entity table>::<entity id>::<title>::<optional: entity url>::<optional: preview file uuid (string)>::<optional: watch list uuid>}}
-
         $params = $insertTag->getParameters();
         $type = $params->getScalar(0);
 
@@ -30,7 +28,18 @@ class AddItemLinkInsertTag implements InsertTagResolverNestedResolvedInterface
             return new InsertTagResult('', OutputType::text);
         }
 
+        return match ($type) {
+            'file' => $this->fileInsertTag($params),
+            'entity' => $this->entityInsertTag($params),
+        };
+    }
 
+    /**
+     * @param ResolvedParameters $params
+     * @return InsertTagResult
+     */
+    private function fileInsertTag(ResolvedParameters $params): InsertTagResult
+    {
         $file = $params->getScalar(1);
         $title = $params->getScalar(2);
         $watchlist = $params->getScalar(3);
@@ -38,10 +47,21 @@ class AddItemLinkInsertTag implements InsertTagResolverNestedResolvedInterface
             $this->watchlistLinkGenerator->generateAddFileLink($file, $title, $watchlist),
             OutputType::html
         );
+    }
 
+    private function entityInsertTag(ResolvedParameters $params): InsertTagResult
+    {
+        $table = $params->getScalar(1);
+        $id = $params->getScalar(2);
+        $title = $params->getScalar(3);
+        $url = $params->getScalar(4);
+        $preview = $params->getScalar(5);
+        $watchlist = $params->getScalar(6);
 
-
-        return  new InsertTagResult('', OutputType::text);
+        return new InsertTagResult(
+            $this->watchlistLinkGenerator->generateEntityLink($table, $id, $title, $url, $preview, $watchlist),
+            OutputType::html
+        );
 
     }
 }
