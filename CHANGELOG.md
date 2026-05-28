36ac477ecfcb2080442ea72ed27d70fe972ce035
Thomas Körner <t.koerner@heimrich-hannot.de>
2026-05-28 15:20:34 +0200

    handle downloads in shared watchlists

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,9 @@
 
 All notable changes to this project will be documented in this file.
 
+## [4.0.0-beta1.2] - 2026-05-28
+- Fixed: download links in shared watchlist not working
+
 ## [4.0.0-beta1.1] - 2026-05-20
 - Changed: allow Uuid in WatchlistLinkGenerator::generateAddFileLink()
 - Fixed: exception when using Uuid with watchlist_add_file twig function
--- src/Controller/FrontendModule/ShareListModuleController.php
+++ src/Controller/FrontendModule/ShareListModuleController.php
@@ -10,15 +10,24 @@ namespace HeimrichHannot\WatchlistBundle\Controller\FrontendModule;
 
 use Contao\CoreBundle\DependencyInjection\Attribute\AsFrontendModule;
 use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController;
+use Contao\CoreBundle\Exception\ResponseException;
+use Contao\CoreBundle\Filesystem\FileDownloadHelper;
+use Contao\CoreBundle\Filesystem\FilesystemItem;
+use Contao\CoreBundle\Filesystem\FilesystemItemIterator;
+use Contao\CoreBundle\Filesystem\VirtualFilesystemInterface;
 use Contao\CoreBundle\Framework\ContaoFramework;
 use Contao\ModuleModel;
 use Contao\Template;
+use HeimrichHannot\WatchlistBundle\Item\WatchlistItem;
 use HeimrichHannot\WatchlistBundle\Item\WatchlistItemFactory;
 use HeimrichHannot\WatchlistBundle\Item\WatchlistItemType;
 use HeimrichHannot\WatchlistBundle\Model\WatchlistModel;
 use HeimrichHannot\WatchlistBundle\Util\WatchlistUtil;
+use HeimrichHannot\WatchlistBundle\Watchlist\WatchlistContentFactory;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\Routing\RouterInterface;
 
 #[AsFrontendModule(ShareListModuleController::TYPE, category: 'miscellaneous', template: 'frontend_modules/watchlist_share_list')]
@@ -31,8 +40,17 @@ class ShareListModuleController extends AbstractFrontendModuleController
         protected WatchlistUtil $watchlistUtil,
         private readonly RouterInterface $router,
         private readonly WatchlistItemFactory $watchlistItemFactory,
+        private readonly FileDownloadHelper $downloadHelper,
+        private readonly VirtualFilesystemInterface $filesStorage,
+        private readonly WatchlistContentFactory $watchlistContentFactory,
     )
     {
+    }
+
+    public function __invoke(Request $request, ModuleModel $model, string $section, ?array $classes = null): Response
+    {
+        $this->handleDownload($request);
+        return parent::__invoke($request, $model, $section, $classes);
     }
 
     protected function getResponse(Template $template, ModuleModel $module, Request $request): Response
@@ -75,5 +93,41 @@ class ShareListModuleController extends AbstractFrontendModuleController
         $template->watchlistDownloadAllUrl = $this->router->generate('huh_watchlist_downlad_all', ['watchlist' => $watchlist->id]);
 
         return $template->getResponse();
+    }
+
+    protected function getFilesystemItems(WatchlistModel $watchlist): FilesystemItemIterator
+    {
+        $content =  $this->watchlistContentFactory->build(
+            $watchlist,
+            pageModel: $this->getPageModel(),
+        );
+
+        $fileItems = array_map(fn(WatchlistItem $item) => $item->getFile(), $content->items);
+        $fileItems = array_values(array_filter($fileItems));
+        return new FilesystemItemIterator($fileItems);
+    }
+
+    protected function handleDownload(Request $request): void
+    {
+        $response = $this->downloadHelper->handle(
+            $request,
+            $this->filesStorage,
+            function (FilesystemItem $item, array $context): Response|null {
+                $watchlist = WatchlistModel::findByPk($context['watchlist'] ?? 0);
+                if (null === $watchlist) {
+                    return new Response('', Response::HTTP_NO_CONTENT);
+                }
+
+                if (!$this->getFilesystemItems($watchlist)->any(static fn (FilesystemItem $listItem) => $listItem->getPath() === $item->getPath())) {
+                    return new Response('The resource can not be accessed anymore.', Response::HTTP_GONE);
+                }
+
+                return null;
+            },
+        );
+
+        if ($response instanceof StreamedResponse || $response instanceof BinaryFileResponse) {
+            throw new ResponseException($response);
+        }
     }
 }
